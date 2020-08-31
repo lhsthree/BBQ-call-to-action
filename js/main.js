3f1a7c0d449636533c1a496b52f860c707edcea3
lhsthree <lhsthree@gmail.com>
2020-08-31 11:26:30 -0500

    feat: adding new message after submitting email and name

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let emailCollectorForm = document.getElementById("Email-Collector");
+emailCollectorForm.addEventListener("submit", event => {
+	event.preventDefault();
+	let ourFormData = new FormData(event.target)
+	
+
+	let userFirstName = ourFormData.get("firstName")
+	let userEmailAddress = ourFormData.get("email")
+	let updatedHTMLContent = `
+
+					<h2>Congratulations!, ${userFirstName}</h2>
+                    
+                    <p>You're on your way to becoming a BBQ master</p>
+                    <p class="fine-print">You will get weekly BBQ tips sent to: ${userEmailAddress}</p>
+                    `
+    let ourMainContent = document.getElementById("registration")
+    ourMainContent.innerHTML = updatedHTMLContent
+
+})
